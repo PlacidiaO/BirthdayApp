5d74d125f0bb0429ffea7a06ae56e2a9263f4160
Placidia <placidia.ong@me.com>
2025-07-25 10:32:43 -0700

    Added friend class and birthday names and options to create new birthdays

--- BirthdayApp/ContentView.swift
+++ BirthdayApp/ContentView.swift
@@ -8,14 +8,48 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var friends:[Friend] = [
+        Friend(name: "Serena", birthday: .now),
+        Friend(name: "Annabelle", birthday: .now),
+        Friend(name: "Lihie", birthday: .now)
+    ]
+    @State private var newName = ""
+    @State private var newBirthday = Date.now
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(friends, id: \.name){ friends in
+                HStack {
+                    Text(friends.name)
+                    Spacer()
+                    Text(friends.birthday, format:
+                        .dateTime.month(.wide).day().year())
+                }
+            }
+            .navigationTitle("Birthdays")
+            .safeAreaInset(edge: .bottom) {
+                VStack(alignment: .center, spacing: 20) {
+                    Text("New Birthday 🎂")
+                        .font(.headline)
+                    DatePicker(selection: $newBirthday, in:
+                        Date.distantPast...Date.now,
+                        displayedComponents: .date) {
+                        TextField("Name", text: $newName)
+                            .textFieldStyle(.roundedBorder)
+                    }
+                
+                    Button("Save") {
+                        let newFriend = Friend(name: newName, birthday: newBirthday)
+                        friends.append(newFriend)
+                        newName = ""
+                        newBirthday = .now
+                    }
+                    .bold()
+                }
+                .padding()
+                .background(.bar)
+            }
         }
-        .padding()
     }
 }
 
---
+++ BirthdayApp/Friends.swift
@@ -0,0 +1,18 @@
+//
+//  Friends.swift
+//  BirthdayApp
+//
+//  Created by Placidia Ong on 7/25/25.
+//
+
+import Foundation
+
+class Friend {
+    var name: String
+    var birthday: Date
+    
+    init(name: String, birthday: Date) {
+        self.name = name
+        self.birthday = birthday
+    }
+}
